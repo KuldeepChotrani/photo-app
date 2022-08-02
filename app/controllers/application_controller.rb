366f1764fd1349698de1412977640280a9ff0242
KuldeepChotrani <kuldeep.chotrani@ignitesol.com>
2022-08-02 12:02:50 +0530

    changes to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
   private
 
     def set_stripe_key
-      Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+      Stripe.api_key = ENV['api_key']
     end
 end
